aa380ad7a4a1de8bfc14892e04d2d64650bef6b2
Alexander Ruziska <ziskapants@gmail.com>
2025-01-10 22:22:35 -0600

    completed basic table functionality

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
-const totalMonthlyCost = 0;
+
+
+function onReady() {
+    console.log('Javascript is working!');
+}
+
+function formHandler(event){
+    event.preventDefault();
+    console.log('form is submitted');
+
+const firstInput = document.getElementById("first");
+const lastInput = document.getElementById("last");
+const idInput = document.getElementById("id");
+const titleInput = document.getElementById("title");
+const annualInput = document.getElementById("annual");
+const data = document.getElementById('table-data');
+console.log(data);
+
+    data.innerHTML += `<tr>
+        <td>${firstInput.value}</td>
+        <td>${lastInput.value}</td>
+        <td>${idInput.value}</td>
+        <td>${titleInput.value}</td>
+        <td>${annualInput.value}</td>
+        <td><button onclick="deleteHandler(event)">Delete</button></td>
+        </tr>`;
+
+firstInput.value = "";
+lastInput.value = "";
+idInput.value = "";
+titleInput.value = "";
+annualInput.value = "";
+
+function calculateMonthlyCost(){
+
+}
+calculateMonthlyCost(data.annualInput.value)
+}
+
+function deleteHandler(event){
+    console.log("row removed");
+    event.target.closest("tr").remove();
+} 
