d69f55434b04562266e8ee04f3c8f99966ac5f4d
Alexander Ruziska <ziskapants@gmail.com>
2025-01-10 23:27:18 -0600

    Monthly total updates correctly

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 function onReady() {
     console.log('Javascript is working!');
 }
@@ -8,36 +6,55 @@ function formHandler(event){
     event.preventDefault();
     console.log('form is submitted');
 
-const firstInput = document.getElementById("first");
-const lastInput = document.getElementById("last");
-const idInput = document.getElementById("id");
-const titleInput = document.getElementById("title");
-const annualInput = document.getElementById("annual");
-const data = document.getElementById('table-data');
-console.log(data);
+    const firstInput = document.getElementById("first");
+    const lastInput = document.getElementById("last");
+    const idInput = document.getElementById("id");
+    const titleInput = document.getElementById("title");
+    const annualInput = document.getElementById("annual");
+    const data = document.getElementById('table-data');
+    console.log(data);
+
+    const annualSalary = Number(annualInput.value);
+    const monthlySalary = annualSalary / 12;
 
     data.innerHTML += `<tr>
         <td>${firstInput.value}</td>
         <td>${lastInput.value}</td>
         <td>${idInput.value}</td>
         <td>${titleInput.value}</td>
-        <td>${annualInput.value}</td>
+        <td data-annual-salary="${annualSalary}">$${annualSalary.toLocaleString()}</td>
         <td><button onclick="deleteHandler(event)">Delete</button></td>
         </tr>`;
 
-firstInput.value = "";
-lastInput.value = "";
-idInput.value = "";
-titleInput.value = "";
-annualInput.value = "";
+    firstInput.value = "";
+    lastInput.value = "";
+    idInput.value = "";
+    titleInput.value = "";
+    annualInput.value = "";
 
-function calculateMonthlyCost(){
-
+    updateTotal();
 }
-calculateMonthlyCost(data.annualInput.value)
+
+function updateTotal(){
+    const salaryCells = document.querySelectorAll('[data-annual-salary]');
+    let totalMonthlyCost = 0;
+
+    salaryCells.forEach(cell => {
+        const annualSalary = cell.getAttribute('data-annual-salary');
+        if (annualSalary) {
+            const monthlySalary = Number(annualSalary) / 12;
+            totalMonthlyCost += monthlySalary;
+        }
+    });
+
+    const paragraph = document.getElementById('total-monthly-cost');
+    if (paragraph) {
+        paragraph.textContent = `Total Monthly Cost: $${totalMonthlyCost.toLocaleString()}`;
+    }
 }
 
 function deleteHandler(event){
     console.log("row removed");
     event.target.closest("tr").remove();
-} 
+    updateTotal();
+}
